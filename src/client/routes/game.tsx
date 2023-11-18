0d40ef082d1d4145e7b2529bddaf5f05d470b3cb
Jack Brown <brown_jack80@hotmail.com>
2023-11-18 23:04:25 +0000

    changes

--- src/client/routes/game.tsx
+++ src/client/routes/game.tsx
@@ -61,6 +61,7 @@ export default function Game() {
 
       if (currentPlayer) utils.player.get.setData(undefined, currentPlayer);
     });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [isPlayerInGame]);
 
   if (gameQuery.isLoading || playerQuery.isLoading)
