4871f0784d146f4683bcfb74b7128e82c89dd5f3
Jack Brown <brown_jack80@hotmail.com>
2023-11-20 10:47:25 +0000

    fix max player count

--- src/server/routers/game.ts
+++ src/server/routers/game.ts
@@ -64,7 +64,7 @@ export const gameRouter = createTRPCRouter({
 
       const playerCount = await redis.scard(`players:${input.gameId}`);
 
-      if (playerCount >= 1)
+      if (playerCount >= 16)
         throw new TRPCError({
           code: 'FORBIDDEN',
           message:
