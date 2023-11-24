3b00e3b655d97745e6e2af68bd2de44f80d091d0
Jack Brown <brown_jack80@hotmail.com>
2023-11-24 09:46:08 +0000

    WIP

--- src/client/components/start-game-dialog.tsx
+++ src/client/components/start-game-dialog.tsx
@@ -1,21 +1,74 @@
+import { zodResolver } from '@hookform/resolvers/zod';
 import { ArrowRight } from 'lucide-react';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
 
 import { Button } from '~/components/ui/button';
 import {
   Dialog,
   DialogContent,
-  DialogDescription,
   DialogFooter,
   DialogHeader,
   DialogTitle,
   DialogTrigger,
 } from '~/components/ui/dialog';
 import { Input } from '~/components/ui/input';
-import { Label } from '~/components/ui/label';
+import { api } from '~/utils/api';
 
-// TODO: Finish this!!
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from './ui/form';
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from './ui/select';
+
+const formSchema = z.object({
+  gameName: z.string({
+    required_error: "Please enter the game's name",
+  }),
+  playerName: z.string({
+    required_error: 'Please enter your name',
+  }),
+  votingSystemValues: z.array(z.string()),
+});
+
+type FormValues = z.infer<typeof formSchema>;
 
 export function StartGameDialog() {
+  const playerQuery = api.player.get.useQuery();
+  const createGame = api.game.create.useMutation();
+
+  const defaultValues: Partial<FormValues> = {
+    playerName: playerQuery.data?.name,
+    votingSystemValues: ['0', '1', '2', '3', '5', '8', '13', '21', '?', '☕️'],
+  };
+
+  const form = useForm<FormValues>({
+    resolver: zodResolver(formSchema),
+    defaultValues,
+    mode: 'onChange',
+  });
+
+  function onSubmit(data: FormValues) {
+    toast({
+      title: 'You submitted the following values:',
+      description: (
+        <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
+          <code className="text-white">{JSON.stringify(data, null, 2)}</code>
+        </pre>
+      ),
+    });
+  }
+
   return (
     <Dialog>
       <DialogTrigger asChild>
@@ -26,35 +79,70 @@ export function StartGameDialog() {
       <DialogContent className="sm:max-w-[425px]">
         <DialogHeader>
           <DialogTitle>Start game</DialogTitle>
-          <DialogDescription>
-            Make changes to your profile here. Click save when you're done.
-          </DialogDescription>
         </DialogHeader>
-        <div className="grid gap-4 py-4">
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="name" className="text-right">
-              Name
-            </Label>
-            <Input
-              id="name"
-              defaultValue="Pedro Duarte"
-              className="col-span-3"
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+            <FormField
+              control={form.control}
+              name="gameName"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Game&apos;s name</FormLabel>
+                  <FormControl>
+                    <Input {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
             />
-          </div>
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="username" className="text-right">
-              Username
-            </Label>
-            <Input
-              id="username"
-              defaultValue="@peduarte"
-              className="col-span-3"
+            <FormField
+              control={form.control}
+              name="playerName"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Your name</FormLabel>
+                  <FormControl>
+                    <Input {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
             />
-          </div>
-        </div>
-        <DialogFooter>
-          <Button type="submit">Save changes</Button>
-        </DialogFooter>
+            <FormField
+              control={form.control}
+              name="votingSystemValues"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Voting system</FormLabel>
+                  <Select
+                    onValueChange={field.onChange}
+                    defaultValue={String(field.value)}
+                  >
+                    <FormControl>
+                      <SelectTrigger>
+                        <SelectValue placeholder="Select a verified email to display" />
+                      </SelectTrigger>
+                    </FormControl>
+                    <SelectContent>
+                      <SelectItem value="m@example.com">
+                        [ '0', '1', '2', '3', '5', '8', '13', '21', '?', '☕️', ]
+                      </SelectItem>
+                      <SelectItem value="m@google.com">m@google.com</SelectItem>
+                      <SelectItem value="m@support.com">
+                        m@support.com
+                      </SelectItem>
+                    </SelectContent>
+                  </Select>
+
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <DialogFooter>
+              <Button type="submit">Let&apos;s go</Button>
+            </DialogFooter>
+          </form>
+        </Form>
       </DialogContent>
     </Dialog>
   );
