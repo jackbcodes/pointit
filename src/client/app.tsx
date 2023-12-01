b29099820577a96063d699a0a6d8e21811abd314
Jack Brown <brown_jack80@hotmail.com>
2023-12-01 16:25:20 +0000

    add lazy loading

--- src/client/app.tsx
+++ src/client/app.tsx
@@ -1,12 +1,16 @@
+import { Suspense, lazy } from 'react';
+
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-import Game from '~/routes/game';
-import Join from '~/routes/join';
-import Root from '~/routes/root';
 import { TRPCProvider, trpcClient } from '~/utils/api';
 
+import { Spinner } from './components/spinner';
 import { ThemeProvider } from './components/theme-provider';
+
+const Root = lazy(() => import('~/routes/root'));
+const Join = lazy(() => import('~/routes/join'));
+const Game = lazy(() => import('~/routes/game'));
 
 const router = createBrowserRouter([
   {
@@ -41,7 +45,9 @@ export default function App() {
     <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
       <TRPCProvider client={trpcClient} queryClient={queryClient}>
         <QueryClientProvider client={queryClient}>
-          <RouterProvider router={router} />
+          <Suspense fallback={<Spinner />}>
+            <RouterProvider router={router} />
+          </Suspense>
         </QueryClientProvider>
       </TRPCProvider>
     </ThemeProvider>
