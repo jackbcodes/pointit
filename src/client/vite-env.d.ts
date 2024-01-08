56d425c3aa28733fccd876fce412a20d46426ab6
Jack Brown <brown_jack80@hotmail.com>
2024-01-08 13:53:01 +0000

    update env example

--- src/client/vite-env.d.ts
+++ src/client/vite-env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_POSTHOG_KEY: string;
+  readonly VITE_POSTHOG_HOST: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
