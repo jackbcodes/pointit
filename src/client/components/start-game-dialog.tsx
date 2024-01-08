03cfa22df8e8c8c6bcfca07d6153b871a91b2002
Jack Brown <brown_jack80@hotmail.com>
2024-01-08 14:05:32 +0000

    remove 34 vote

--- src/client/components/start-game-dialog.tsx
+++ src/client/components/start-game-dialog.tsx
@@ -130,7 +130,7 @@ export function StartGameDialog({
                     <VotingSystemRadioGroupItem
                       value="fibonacci"
                       name="Fibonacci"
-                      values="0, 1, 2, 3, 5, 8, 13, 21, 34, ?, ☕️"
+                      values="0, 1, 2, 3, 5, 8, 13, 21, ?, ☕️"
                     />
                     <VotingSystemRadioGroupItem
                       value="t-shirt"
