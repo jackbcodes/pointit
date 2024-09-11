fab6b9718972d35e81d73155a0faa7f422f9720d
Jack Brown <brown_jack80@hotmail.com>
2024-09-11 10:02:20 +0100

    chore: temporarily remove eslint

--- src/server/routers/user.ts
+++ src/server/routers/user.ts
@@ -1,6 +1,5 @@
 import { router, publicProcedure } from '~/utils/trpc';
 
 export const userRouter = router({
-  // eslint-disable-next-line unicorn/no-null -- react-query doesn't allow sending undefined
   get: publicProcedure.query(({ ctx }) => ctx.user ?? null),
 });
